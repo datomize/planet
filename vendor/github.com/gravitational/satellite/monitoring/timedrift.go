eeed48bac8dcd63f7571bc8a623d7a6b8eda2acd
Bernard Kim <bernard@goteleport.com>
2021-02-01 16:33:45 -0800

    Revendor satellite

--- vendor/github.com/gravitational/satellite/monitoring/timedrift.go
+++ vendor/github.com/gravitational/satellite/monitoring/timedrift.go
@@ -277,7 +277,7 @@ func (c *timeDriftChecker) removeExpiredClients(members []*pb.MemberStatus) {
 }
 
 // shouldCheckNode returns true if the check should be run against specified
-// serf member.
+// member.
 func (c *timeDriftChecker) shouldCheckNode(node *pb.MemberStatus) bool {
 	return node.Status == pb.MemberStatus_Alive && c.NodeName != node.Name
 }
